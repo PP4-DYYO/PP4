4a9c55006c7d92a5b1f128564f4fa0f59f7a40f7
ifeat <ifeatablems@outlook.jp>
2018-09-22 17:57:47 +0900

    [upgrade]武装シーンの追加
    武装専用のUIレイヤー追加、武装クラス追加、簡単な武装クラス実装、武装専用のPrefab追加、

---
+++ PP4/Assets/Scene/Armed/Script/MyArmedUi.cs
@@ -0,0 +1,54 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//2017/9/22～
+//作成者　京都コンピュータ学院京都駅前校　ゲーム学科　4回生　奥田裕也
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 武装UI
+/// </summary>
+public class MyArmedUi : MonoBehaviour
+{
+	/// <summary>
+	/// ゲームスタートボタン
+	/// </summary>
+	[SerializeField]
+	Button GameStartButton;
+
+	/// <summary>
+	/// 武装するキャンバス
+	/// </summary>
+	[SerializeField]
+	MyArmedCanvas ArmedCanvas;
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 初期
+	/// </summary>
+	void Start()
+	{
+		//武装メニューの表示
+		ArmedCanvas.Display();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// ゲームを開始
+	/// </summary>
+	public void GameStart()
+	{
+		Debug.Log("ゲームスタート");
+	}
+}
---
+++ PP4/Assets/Scene/Script/MyArmedCanvas.cs
@@ -0,0 +1,56 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//2017/9/22～
+//作成者　京都コンピュータ学院京都駅前校　ゲーム学科　4回生　奥田裕也
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 武装のキャンバス
+/// </summary>
+public class MyArmedCanvas : MonoBehaviour
+{
+	/// <summary>
+	/// 武装用のカメラ
+	/// </summary>
+	[SerializeField]
+	Camera ArmedCamera;
+	
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 起動
+	/// </summary>
+	void Awake()
+	{
+		ArmedCamera.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 表示
+	/// </summary>
+	public void Display()
+	{
+		ArmedCamera.enabled = true;
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// プレイヤー名を保存
+	/// </summary>
+	public void SavePlayerName()
+	{
+		Debug.Log("プレイヤー名を保存しました。");
+	}
+}

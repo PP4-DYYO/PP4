147032ea1a2e331cb518879aa6d8cf060052d0b8
JunkiYoshida <jun_y0726@yahoo.co.jp>
2018-12-24 20:11:46 +0900

    [upgrade]クレジットの調整
    動きの調整、自動で進行するように変更、雲の変化の種類を追加予定

--- PP4/Assets/Scene/Credit/Script/MyCreditManager.cs
+++ PP4/Assets/Scene/Credit/Script/MyCreditManager.cs
@@ -5,6 +5,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,6 +22,38 @@ public class MyCreditManager : MonoBehaviour
 	/// 背景の位置
 	/// </summary>
 	Vector3 BackGroundPosition;
+
+	/// <summary>
+	/// 終わりの画面切り替え用のレンダラー
+	/// </summary>
+	[SerializeField]
+	SpriteRenderer EndingImageRenderer;
+
+	/// <summary>
+	/// 終わりの画面切り替え用のレンダラーの色
+	/// </summary>
+	Color EndingImageColor;
+
+	/// <summary>
+	/// 終わりの画面切り替え用のレンダラーα値
+	/// </summary>
+	float m_endingImageRendererAlpha;
+
+	/// <summary>
+	/// 乱数用
+	/// </summary>
+	int m_randomNum;
+
+	/// <summary>
+	/// イベント開始待ち時間
+	/// </summary>
+	public float m_waitingTime;
+
+	/// <summary>
+	/// イベント開始の間隔
+	/// </summary>
+	[SerializeField]
+	float m_textIntervalTime;
 
 	/// <summary>
 	/// 中央のキャラクター
@@ -50,6 +83,38 @@ public class MyCreditManager : MonoBehaviour
 	/// </summary>
 	[SerializeField]
 	GameObject AxisObject;
+
+	/// <summary>
+	/// 楕円運動用度数
+	/// </summary>
+	float m_degree;
+
+	/// <summary>
+	/// m_degreeの値をラジアンに変換するための定数
+	/// </summary>
+	const float DEGREE_TO_RADIAN = Mathf.PI / 180.0f;
+
+	/// <summary>
+	/// m_degreeの値をラジアンに変換した値
+	/// </summary>
+	float m_radian;
+
+	/// <summary>
+	/// 楕円運動のX方向の半径
+	/// </summary>
+	[SerializeField]
+	float m_radiusX;
+
+	/// <summary>
+	/// 楕円運動のZ方向の半径
+	/// </summary>
+	[SerializeField]
+	float m_radiusZ;
+
+	/// <summary>
+	/// 角度の最大
+	/// </summary>
+	const float ANGLE_MAX= 360;
 
 	/// <summary>
 	/// オブジェクトの移動速度
@@ -101,6 +166,12 @@ public class MyCreditManager : MonoBehaviour
 	/// </summary>
 	[SerializeField]
 	GameObject CloudRightEndObject;
+
+	/// <summary>
+	/// 画面の上端の位置
+	/// </summary>
+	[SerializeField]
+	GameObject DisplayUpOutsideObject;
 
 	/// <summary>
 	/// 画面外の位置
@@ -168,6 +239,11 @@ public class MyCreditManager : MonoBehaviour
 	bool m_eventPlaying;
 
 	/// <summary>
+	/// 全ての表示が終わった
+	/// </summary>
+	bool m_ending;
+
+	/// <summary>
 	/// 隕石のオブジェクト
 	/// </summary>
 	[SerializeField]
@@ -207,7 +283,7 @@ public class MyCreditManager : MonoBehaviour
 	float ThunderRendererAlpha;
 
 	// <summary>
-	/// 最大のα値
+	/// α値の最大
 	/// </summary>
 	const float ALPHA_MAX = 1;
 
@@ -260,7 +336,7 @@ public class MyCreditManager : MonoBehaviour
 	bool m_lightningStruck;
 
 	/// <summary>
-	/// 雷用の2
+	/// 雷用の数字の2
 	/// </summary>
 	const int THUNDER_TWICE = 2;
 
@@ -316,7 +392,7 @@ public class MyCreditManager : MonoBehaviour
 	/// <summary>
 	/// 雲の状態
 	/// </summary>
-	CloudState cloudState;
+	CloudState m_cloudState;
 
 	/// <summary>
 	/// 雲が消える原因列挙
@@ -344,7 +420,7 @@ public class MyCreditManager : MonoBehaviour
 	/// <summary>
 	/// 雲が消える原因
 	/// </summary>
-	CloudDestroyCause cloudDestroyCause;
+	CloudDestroyCause m_cloudDestroyCause;
 
 	//----------------------------------------------------------------------------------------------------
 	/// <summary>
@@ -361,6 +437,9 @@ public class MyCreditManager : MonoBehaviour
 
 		ThunderColor = ThunderRenderer.GetComponent<SpriteRenderer>().color;
 		ThunderRendererAlpha = ThunderRenderer.color.a;
+
+		EndingImageColor = EndingImageRenderer.GetComponent<SpriteRenderer>().color;
+		m_endingImageRendererAlpha = EndingImageRenderer.color.a;
 	}
 
 	//----------------------------------------------------------------------------------------------------
@@ -376,7 +455,7 @@ public class MyCreditManager : MonoBehaviour
 		CreditCharacterMoving();
 
 		//状況の確認
-		CheckEventPlay();
+		CheckPlayEvent();
 
 		//背景の移動
 		BackGroundMoving();
@@ -387,27 +466,80 @@ public class MyCreditManager : MonoBehaviour
 		//テキストの移動
 		PlayTextEvent();
 
-		//テキスト表示イベントを開始する
+		//テキスト表示イベント
+		PlayEvent();
 
-		if (Input.GetKeyDown(KeyCode.Space) && !m_eventPlaying)
-		{
-			cloudDestroyCause = CloudDestroyCause.Jet;
-			cloudState = CloudState.Generate;
-		}
-		if (Input.GetKeyDown(KeyCode.Return) && !m_eventPlaying)
-		{
-			cloudDestroyCause = CloudDestroyCause.Meteorite;
-			cloudState = CloudState.Generate;
-		}
-		if (Input.GetKeyDown(KeyCode.T) && !m_eventPlaying)
-		{
-			cloudDestroyCause = CloudDestroyCause.Thunder;
-			cloudState = CloudState.Generate;
-		}
+		//キー入力判定
+		MyInputChecker();
+	}
 
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// キー入力
+	/// </summary>
+	void MyInputChecker()
+	{
 		if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetButtonDown("BackButton"))
 		{
-			MySceneManager.Instance.ChangeScene(MyScene.Title);
+			m_ending = true;
+			Time.timeScale = 1;
+		}
+
+		if (!m_ending)
+		{
+			if (Input.GetKey(KeyCode.Space))
+				Time.timeScale = 5;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			Time.timeScale = 1;
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 進行イベント
+	/// </summary>
+	void PlayEvent()
+	{
+		if (!m_eventPlaying && m_textNum < MAX_TEXT_NUM)
+		{
+			if (m_cloudState != CloudState.Generate)
+			{
+				m_cloudState = CloudState.Generate;
+			}
+
+			m_waitingTime = m_waitingTime + Time.deltaTime;
+			if (m_waitingTime > m_textIntervalTime)
+			{
+				m_randomNum = UnityEngine.Random.Range(0, 3);
+				switch (m_randomNum)
+				{
+					case 0:
+						m_cloudDestroyCause = CloudDestroyCause.Jet;
+						break;
+					case 1:
+						m_cloudDestroyCause = CloudDestroyCause.Meteorite;
+						break;
+					case 2:
+						m_cloudDestroyCause = CloudDestroyCause.Thunder;
+						break;
+				}
+				m_waitingTime = 0;
+			}
+		}
+
+		if (m_ending)
+		{
+			m_endingImageRendererAlpha = m_endingImageRendererAlpha + (m_ChangeAlphaSpeed * Time.deltaTime);
+			EndingImageColor.a = m_endingImageRendererAlpha;
+			EndingImageRenderer.color = EndingImageColor;
+			if (m_endingImageRendererAlpha >= 1)
+			{
+				Time.timeScale = 1;
+				MySceneManager.Instance.ChangeScene(MyScene.Title);
+			}
 		}
 	}
 
@@ -415,9 +547,9 @@ public class MyCreditManager : MonoBehaviour
 	/// <summary>
 	/// 進行状況の確認
 	/// </summary>
-	void CheckEventPlay()
+	void CheckPlayEvent()
 	{
-		if (cloudDestroyCause != CloudDestroyCause.Non)
+		if (m_cloudDestroyCause != CloudDestroyCause.Non)
 		{
 			m_eventPlaying = true;
 		}
@@ -433,7 +565,7 @@ public class MyCreditManager : MonoBehaviour
 	/// </summary>
 	void CloudEvent()
 	{
-		switch (cloudState)
+		switch (m_cloudState)
 		{
 			//雲が生成される
 			case CloudState.Generate:
@@ -446,12 +578,14 @@ public class MyCreditManager : MonoBehaviour
 				{
 					ParticleCloud.Pause();
 				}
+				if (CloudObject.transform.position.y > TextStartPositionObject.transform.position.y)
+					CloudObject.transform.position = new Vector3(CloudObject.transform.position.x, CloudObject.transform.position.y - (10f * Time.deltaTime), CloudObject.transform.position.z);
 				break;
 
 			//雲が動く
 			case CloudState.Play:
 				ParticleCloud.Play();
-				cloudState = CloudState.Progress;
+				m_cloudState = CloudState.Progress;
 				break;
 
 			//雲が動いている
@@ -459,14 +593,15 @@ public class MyCreditManager : MonoBehaviour
 				m_cloudTime += Time.deltaTime;
 				if (m_cloudTime >= CLOUD_LIMIT_TIME)
 				{
-					cloudState = CloudState.Destroy;
+					m_cloudState = CloudState.Destroy;
 					m_cloudTime = 0;
+					CloudObject.transform.position = new Vector3(CloudObject.transform.position.x, DisplayUpOutsideObject.transform.position.y, CloudObject.transform.position.z);
 				}
 				break;
 
 			//雲が消えた
 			case CloudState.Destroy:
-				cloudState = CloudState.Non;
+				m_cloudState = CloudState.Non;
 				m_textMoving = true;
 				break;
 		}
@@ -478,7 +613,7 @@ public class MyCreditManager : MonoBehaviour
 	/// </summary>
 	void PlayTextEvent()
 	{
-		switch (cloudDestroyCause)
+		switch (m_cloudDestroyCause)
 		{
 			case CloudDestroyCause.Jet:
 				CloudJetDestroy();
@@ -512,7 +647,7 @@ public class MyCreditManager : MonoBehaviour
 		//停止位置に着くとリセットする
 		if (CreditText[m_textNum].transform.position.y <= TextStopPositionObject.transform.position.y)
 		{
-			switch (cloudDestroyCause)
+			switch (m_cloudDestroyCause)
 			{
 				case CloudDestroyCause.Jet:
 					m_leftCharacterMoved = false;
@@ -528,10 +663,14 @@ public class MyCreditManager : MonoBehaviour
 					m_lightningStruck = false;
 					break;
 			}
-			cloudState = CloudState.Non;
-			cloudDestroyCause = CloudDestroyCause.Non;
+			m_cloudState = CloudState.Non;
+			m_cloudDestroyCause = CloudDestroyCause.Non;
 			m_textChanged = false;
 			m_textMoving = false;
+			if (m_textNum == MAX_TEXT_NUM)
+			{
+				m_ending = true;
+			}
 		}
 	}
 
@@ -546,10 +685,6 @@ public class MyCreditManager : MonoBehaviour
 			m_textNum++;
 			CreditText[m_textNum].transform.position = TextStartPositionObject.transform.position;
 			m_textChanged = true;
-		}
-		else
-		{
-			Debug.Log("クレジット表示終了");
 		}
 	}
 
@@ -574,7 +709,7 @@ public class MyCreditManager : MonoBehaviour
 			//雲の右端で雲が動き始める
 			if (LeftCharacterPosition.z >= CloudRightEndObject.transform.position.z)
 			{
-				cloudState = CloudState.Play;
+				m_cloudState = CloudState.Play;
 			}
 
 			//画面外へ着くと初期位置へ戻す
@@ -608,7 +743,7 @@ public class MyCreditManager : MonoBehaviour
 			//隕石が雲の位置に着くと雲が分裂（現在は消える）
 			if (Meteorite.transform.position.y <= CloudObject.transform.position.y)
 			{
-				cloudState = CloudState.Play;
+				m_cloudState = CloudState.Play;
 			}
 
 			//隕石が停止位置に着くと初期位置に戻りテキストの移動を始める
@@ -626,68 +761,75 @@ public class MyCreditManager : MonoBehaviour
 	/// </summary>
 	void CloudThunderDestroy()
 	{
-		if (!m_textMoving)
+		m_thunderTime++;
+
+		//表示するテキストを変更
+		if (!m_textChanged)
 		{
-			m_thunderTime++;
+			TextChange();
+		}
 
-			//表示するテキストを変更
-			if (!m_textChanged)
+		if (!m_lightningStruck)
+		{
+			//雷が落ちるまで点滅する
+			if (m_thunderFlashCount < THUNDER_FLASH_COUNT_LIMIT)
 			{
-				TextChange();
-			}
-
-			if (!m_lightningStruck)
-			{
-				//雷が落ちるまで点滅する
-				if (m_thunderFlashCount < THUNDER_FLASH_COUNT_LIMIT)
+				if (m_thunderTime % THUNDER_FLASH_INTERVAL == 0 && m_thunderFlashCount % THUNDER_TWICE == 0)
 				{
-					if (m_thunderTime % THUNDER_FLASH_INTERVAL == 0 && m_thunderFlashCount % THUNDER_TWICE == 0)
-					{
-						ThunderFlash.SetActive(true);
-						m_thunderFlashCount++;
-					}
-					if (m_thunderTime % (THUNDER_FLASH_INTERVAL * THUNDER_TWICE) == 0 && m_thunderFlashCount % THUNDER_TWICE != 0)
-					{
-						ThunderFlash.SetActive(false);
-						m_thunderFlashCount++;
-					}
+					ThunderFlash.SetActive(true);
+					m_thunderFlashCount++;
 				}
-				else
+				if (m_thunderTime % (THUNDER_FLASH_INTERVAL * THUNDER_TWICE) == 0 && m_thunderFlashCount % THUNDER_TWICE != 0)
 				{
-					//雷が落ちる（稲妻が出現する）
-					Thunder.SetActive(true);
-					m_lightningStruck = true;
-					//落ちた時に雲は消え始める
-					cloudState = CloudState.Play;
+					ThunderFlash.SetActive(false);
+					m_thunderFlashCount++;
 				}
 			}
 			else
 			{
-				//雷が落ちた後、稲妻の透明度を下げていく
-				ThunderRendererAlpha -= m_ChangeAlphaSpeed;
-				ThunderColor.a = ThunderRendererAlpha;
-				ThunderRenderer.color = ThunderColor;
-
-				if (ThunderRendererAlpha <= 0)
-				{
-					ThunderRendererAlpha = ALPHA_MAX;
-					ThunderColor.a = ThunderRendererAlpha;
-
-					ThunderRenderer.color = ThunderColor;
-
-					Thunder.SetActive(false);
-				}
+				//雷が落ちる（稲妻が出現する）
+				Thunder.SetActive(true);
+				m_lightningStruck = true;
+				//落ちた時に雲は消え始める
+				m_cloudState = CloudState.Play;
 			}
 		}
+		else
+		{
+			//雷が落ちた後、稲妻の透明度を下げていく
+			ThunderRendererAlpha = ThunderRendererAlpha - (m_ChangeAlphaSpeed * Time.deltaTime);
+			ThunderColor.a = ThunderRendererAlpha;
+			ThunderRenderer.color = ThunderColor;
+
+			if (ThunderRendererAlpha <= 0)
+			{
+				ThunderRendererAlpha = ALPHA_MAX;
+				ThunderColor.a = ThunderRendererAlpha;
+
+				ThunderRenderer.color = ThunderColor;
+
+				Thunder.SetActive(false);
+			}
+		}
+
 	}
 
 	//----------------------------------------------------------------------------------------------------
 	/// <summary>
-	/// 中央のキャラクターの移動
+	/// 中央のキャラクターの移動(楕円運動)
 	/// </summary>
 	void CreditCharacterMoving()
 	{
-		CreditCharacter.transform.RotateAround(AxisObject.transform.position, -Vector3.up, m_creditCharacterSpeed);
+		// 移動する分の値をm_degreeに入れる
+		m_degree += m_creditCharacterSpeed * Time.deltaTime;
+
+		// m_degreeの値を0から360までの間の数値に変換する
+		m_degree = (m_degree % ANGLE_MAX + ANGLE_MAX) % ANGLE_MAX;
+
+		// m_degreeの値をラジアンに変換した値をm_radianに入れる
+		m_radian = m_degree * DEGREE_TO_RADIAN;
+
+		CreditCharacter.transform.position = new Vector3(AxisObject.transform.position.x + Mathf.Cos(m_radian) * m_radiusX, 0, AxisObject.transform.position.z + Mathf.Sin(m_radian) * m_radiusZ);
 		CreditCharacter.transform.LookAt(AxisObject.transform.position);
 	}
 

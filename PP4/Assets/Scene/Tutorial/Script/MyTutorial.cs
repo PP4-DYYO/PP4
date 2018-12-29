c967d1b23fe4d14f4bc96feed7c7d7298e0112b9
JunkiYoshida <jun_y0726@yahoo.co.jp>
2018-12-30 02:06:57 +0900

    [upgrade]タイトルとチュートリアルの更新
    タイトルシーンにボタン操作を表示、チュートリアルのミッション機能のみ実装

--- PP4/Assets/Scene/Tutorial/Script/MyTutorial.cs
+++ PP4/Assets/Scene/Tutorial/Script/MyTutorial.cs
@@ -6,6 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 using UnityEngine;
+using UnityEngine.UI;
 
 //----------------------------------------------------------------------------------------------------
 /// <summary>
@@ -15,12 +16,33 @@ public class MyTutorial : MyGame
 {
 	int[] m_ranking = new int[8];
 	int[] m_height = new int[8];
+
 	[SerializeField]
 	GameObject PlayerObject;
 
 	int myRankNum;
 
 	int nowRank;
+
+	struct Mission
+	{
+		public string m_text;
+		public bool m_clear;
+	}
+
+	Mission[] TutorialMission = new Mission[3];
+
+	[SerializeField]
+	GameObject[] ClearObject = new GameObject[3];
+
+	[SerializeField]
+	Text[] MissionText = new Text[3];
+
+	[SerializeField]
+	Text TimerText;
+
+	[SerializeField]
+	float m_tutorialTime;
 
 	//----------------------------------------------------------------------------------------------------
 	/// <summary>
@@ -53,16 +75,71 @@ public class MyTutorial : MyGame
 		m_height[5] = 200;
 		m_height[6] = 100;
 		m_height[7] = (int)PlayerObject.transform.position.y;
+
+		TutorialMission[0].m_text = "３００mに到達せよ";
+		TutorialMission[1].m_text = "タンクの水を空にせよ";
+		TutorialMission[2].m_text = "コインを１０枚所得せよ";
+
+		MissionText[0].text = TutorialMission[0].m_text + "\n";
+		MissionText[1].text = TutorialMission[1].m_text + "\n";
+		MissionText[2].text = TutorialMission[2].m_text + "\n";
+
+		TimerText.text = "" + m_tutorialTime;
 	}
 
 	//----------------------------------------------------------------------------------------------------
 	/// <summary>
-	///定期 
+	/// ミッションの判定
+	/// </summary>
+	void MissionCheck()
+	{
+		if (PlayerObject.transform.position.y > 100)
+		{
+			TutorialMission[0].m_clear = true;
+			MissionText[0].color = Color.gray;
+			ClearObject[0].SetActive(true);
+		}
+
+		if (OperatingPlayer.GetPercentageOfRemainingWater() == 0)
+		{
+			TutorialMission[1].m_clear = true;
+			MissionText[1].color = Color.gray;
+			ClearObject[1].SetActive(true);
+		}
+
+		if (OperatingPlayer.NumOfCoins >= 10)
+		{
+			TutorialMission[2].m_clear = true;
+			MissionText[2].color = Color.gray;
+			ClearObject[2].SetActive(true);
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	///定期更新
 	/// </summary>
 	void Update()
 	{
 		//水の残量表示
 		MainUi.SetRemainingAmountOfWater(OperatingPlayer.GetPercentageOfRemainingWater());
+
+		//コイン所持数表示
+		MainUi.SetNumOfCoins(OperatingPlayer.NumOfCoins);
+
+		//ミッションの判定
+		MissionCheck();
+
+		if (m_tutorialTime > 0)
+		{
+			m_tutorialTime -= Time.deltaTime;
+		}
+		else
+		{
+			m_tutorialTime = 0;
+			MySceneManager.Instance.ChangeScene(MyScene.Title);
+		}
+		TimerText.text = m_tutorialTime.ToString("f2");
 
 		//残量０で落下開始
 		if (OperatingPlayer.GetPercentageOfRemainingWater() == 0)
@@ -74,9 +151,6 @@ public class MyTutorial : MyGame
 		{
 			MainUi.StopOfFall();
 		}
-
-		//コイン所持数表示
-		MainUi.SetNumOfCoins(OperatingPlayer.NumOfCoins);
 
 		if (PlayerObject.transform.position.y > m_height[6] && nowRank > 7)
 		{

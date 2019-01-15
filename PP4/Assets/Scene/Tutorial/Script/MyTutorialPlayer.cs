eb0f80906782d12c68728de198739265e8af0060
JunkiYoshida <jun_y0726@yahoo.co.jp>
2019-01-15 15:39:07 +0900

    [upgrade]チュートリアルの更新
    敵キャラクターが飛ぶ、プレイヤーが水を被れるように

---
+++ PP4/Assets/Scene/Tutorial/Script/MyTutorialPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTutorialPlayer : MonoBehaviour
+{
+
+	[SerializeField]
+	MyMainUi MainUiScript;
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 当たり判定
+	/// </summary>
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == SplasheInfo.TAG || other.tag == SplasheInfo.TRANS_TAG)
+		{
+			MainUiScript.WearWater();
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// 当たり続ける判定
+	/// </summary>
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == SplasheInfo.TAG || other.tag == SplasheInfo.TRANS_TAG)
+		{
+			MainUiScript.WearWater();
+		}
+	}
+}
